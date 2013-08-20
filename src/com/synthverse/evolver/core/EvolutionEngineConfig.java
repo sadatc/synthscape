03a8fc4579d582e67a6678a5d608f27e45ddb398
Sadat Chowdhury <sadatc@gmail.com>
2013-08-20 19:02:36 -0400

    changed constants

--- src/com/synthverse/evolver/core/EvolutionEngineConfig.java
+++ src/com/synthverse/evolver/core/EvolutionEngineConfig.java
@@ -7,30 +7,28 @@ package com.synthverse.evolver.core;
  */
 public interface EvolutionEngineConfig {
 
-    public static final double DEFAULT_PERCENT_TOP = 0.3;
+    public static final double EE_DEF_PERCENT_TOP = 0.3;
 
-    public static final double DEFAULT_PERCENT_TOP_X_TOP = 0.25;
+    public static final double EE_DEF_PERCENT_TOP_X_TOP = 0.25;
 
-    public static final double DEFAULT_PERCENT_TOP_MUTANT = 0.20;
+    public static final double EE_DEF_PERCENT_TOP_MUTANT = 0.20;
 
-    public static final double DEFAULT_PERCENT_TOP_X_BOTTOM = 0.05;
+    public static final double EE_DEF_PERCENT_TOP_X_BOTTOM = 0.05;
 
-    public static final double DEFAULT_PERCENT_BOTTOM = 0.05;
+    public static final double EE_DEF_PERCENT_BOTTOM = 0.05;
 
-    public static final double DEFAULT_PERCENT_BOTTOM_MUTANT = 0.05;
+    public static final double EE_DEF_PERCENT_BOTTOM_MUTANT = 0.05;
 
-    public static final double DEFAULT_PERCENT_BOTTOM_X_BOTTOM = 0.05;
+    public static final double EE_DEF_PERCENT_BOTTOM_X_BOTTOM = 0.05;
 
-    public static final double DEFAULT_PERCENT_RANDOM = 0.05;
+    public static final double EE_DEF_PERCENT_RANDOM = 0.05;
 
-    public static final int DEFAULT_MAX_ENTITY_SIZE = 32;
+    public static final int EE_DEF_MAX_ENTITY_SIZE = 32;
 
-    public static final int DEFAULT_GENE_POOL_SIZE = 100;
+    public static final int EE_DEF_GENE_POOL_SIZE = 100;
 
-    public static final int DEFAULT_MAX_GENERATIONS = 10;
+    public static final double EE_DEF_MAX_MUTATION_RATE = 0.05;
 
-    public static final double DEFAULT_MAX_MUTATION_RATE = 0.05;
-
-    public static final String DEFAULT_EVOLUTION_PROGRESS_LOG = "/tmp/progress.log";
+    public static final String EE_DEF_EVOLUTION_PROGRESS_LOG = "/tmp/progress.log";
 
 }
