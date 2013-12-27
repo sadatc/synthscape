0692c4559d0cf7c401b9a2670e3f2b5f24bb515f
Sadat Chowdhury <sadatc@gmail.com>
2013-12-27 18:49:06 -0500

    removed unneccessary libs

--- src/com/synthverse/synthscape/experiment/dissertation/islands/PopulationIslandSimulation.java
+++ src/com/synthverse/synthscape/experiment/dissertation/islands/PopulationIslandSimulation.java
@@ -1,8 +1,6 @@
 package com.synthverse.synthscape.experiment.dissertation.islands;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import sim.util.Int2D;
