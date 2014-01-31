cd664777d0464b326b491178f2a9974a5faca48f
Sadat Chowdhury <sadatc@gmail.com>
2014-01-30 20:00:10 -0500

    added team mechanism

---
+++ src/com/synthverse/synthscape/core/Team.java
@@ -0,0 +1,89 @@
+package com.synthverse.synthscape.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+    private int teamId;
+    protected double fitness;
+    List<Agent> members;
+    boolean allMembersEvaluated = false;
+    int expectedSize = 0;
+    
+    
+    public Team() {
+	init();
+    }
+
+    public void init() {
+	teamId = -1;
+	fitness = 0;
+	if (members == null) {
+	    members = new ArrayList<Agent>();
+	} else {
+	    members.clear();
+	}
+	allMembersEvaluated = false;
+    }
+
+    public int getTeamId() {
+	return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+	this.teamId = teamId;
+    }
+
+    public double getFitness() {
+	return fitness;
+    }
+
+    public void setFitness(double fitness) {
+	this.fitness = fitness;
+    }
+
+    public List<Agent> getMembers() {
+	return members;
+    }
+
+    public void clearMembers() {
+	if (members != null) {
+	    members.clear();
+	}
+	allMembersEvaluated = false;
+    }
+
+    public void addMember(Agent agent) {
+	members.add(agent);
+    }
+
+    public double getMaxFitness() {
+	double maxFitness = 0;
+	for (Agent member : members) {
+	    double memberFitness = member.getFitness();
+	    if (memberFitness > maxFitness) {
+		maxFitness = memberFitness;
+	    }
+	}
+	return maxFitness;
+    }
+    
+    public void setAllMembersEvaluated(boolean value) {
+	allMembersEvaluated = value;
+    }
+
+    public boolean isAllMembersEvaluated() {
+	return allMembersEvaluated;
+    }
+
+    public int getExpectedSize() {
+        return expectedSize;
+    }
+
+    public void setExpectedSize(int expectedSize) {
+        this.expectedSize = expectedSize;
+    }
+    
+    
+    
+}
