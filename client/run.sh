c2fc288c428343882a85bdfb495a3862638758e5
U-DEVSERVER\Administrator <Administrator@SYMMETRY.devserver.local>
2014-03-18 15:25:21 -0400

    modified scripts

--- client/run.sh
+++ client/run.sh
@@ -1,1 +1,5 @@
-java -cp "../lib/commons-math3-3.0.jar:../mason/mason.17.jar:." com.synthverse.synthscape.experiment.dissertation.islands.PopulationIslandSimulation
+cd bin
+#java -cp "../../lib/commons-math3-3.0.jar:../../mason/mason.17.jar:." com.synthverse.synthscape.experiment.dissertation.islands.PopulationIslandSimulation
+pwd
+java  -cp ".:../../lib/commons-math3-3.0.jar:../../mason/mason.17.jar" com.synthverse.synthscape.experiment.dissertation.islands.PopulationIslandSimulation
+cd ..
---
+++ client/runcygwin.sh
@@ -0,0 +1,3 @@
+cd bin
+java  -cp ".;..\..\lib\commons-math3-3.0.jar;..\..\mason\mason.17.jar" com.synthverse.synthscape.experiment.dissertation.islands.PopulationIslandSimulation
+cd ..
