0d6741a2871592e24bfe8d4e51f271cb6a0bc626
Sadat Chowdhury <sadatc@gmail.com>
2014-03-20 20:50:17 -0400

    basic ant setup completed

--- src/com/synthverse/Main.java
+++ src/com/synthverse/Main.java
@@ -1,10 +1,12 @@
 package com.synthverse;
 
 import com.synthverse.synthscape.core.D;
+import com.synthverse.synthscape.experiment.dissertation.islands.PopulationIslandSimulation;
 
 public class Main {
     public static void main(String[] arg) {
 	D.p("WELCOME");
+	PopulationIslandSimulation.main(arg);
     }
 
 }
