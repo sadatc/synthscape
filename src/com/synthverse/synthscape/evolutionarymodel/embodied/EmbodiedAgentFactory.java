d5a15c5387a299457782c9d85dc62267e1112186
Sadat Chowdhury <sadatc@gmail.com>
2014-04-03 20:34:35 -0400

    corrections

--- src/com/synthverse/synthscape/evolutionarymodel/embodied/EmbodiedAgentFactory.java
+++ src/com/synthverse/synthscape/evolutionarymodel/embodied/EmbodiedAgentFactory.java
@@ -1,27 +1,28 @@
 package com.synthverse.synthscape.evolutionarymodel.embodied;
 
+import com.synthverse.Main;
 import com.synthverse.synthscape.core.Agent;
 import com.synthverse.synthscape.core.AgentFactory;
 import com.synthverse.synthscape.core.Constants;
 import com.synthverse.synthscape.core.Simulation;
 import com.synthverse.synthscape.core.Species;
-import com.synthverse.synthscape.evolutionarymodel.islands.IslanderAgent;
 
 public class EmbodiedAgentFactory extends AgentFactory implements Constants {
-    
 
     protected EmbodiedAgentFactory(Simulation simulation) {
 	super(simulation);
     }
 
-    @Override
-    public Agent createNewFactoryAgent(Species species) {
-	IslanderAgent agent = new IslanderAgent(simulation, species);
+    public Agent createNewFactoryAgent(Species species, int poolSize) {
+	EmbodiedAgent agent = new EmbodiedAgent(simulation, species, poolSize);
 	agent.setMaxSteps(simulation.getMaxStepsPerAgent());
 	agent.setInteractionMechanisms(simulation.getInteractionMechanisms());
-	
-	
 	return agent;
+    }
+
+    @Override
+    public Agent createNewFactoryAgent(Species species) {
+	return createNewFactoryAgent(species, Main.settings.DEFAULT_EMBODIED_AGENT_POOL_SIZE);
     }
 
 }
