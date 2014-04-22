b75f401b4898e12b52bb009d114da1802510d146
Sadat Chowdhury <sadatc@gmail.com>
2014-04-22 19:37:37 -0400

    standardized logger

--- src/com/synthverse/Main.java
+++ src/com/synthverse/Main.java
@@ -7,10 +7,14 @@ import com.synthverse.synthscape.core.EvolutionaryModel;
 import com.synthverse.synthscape.core.Settings;
 import com.synthverse.synthscape.evolutionarymodel.embodied.EmbodiedEvolutionSimulation;
 import com.synthverse.synthscape.evolutionarymodel.islands.PopulationIslandSimulation;
+import com.synthverse.util.LogUtils;
 
 public class Main {
     public static Settings settings = Settings.getInstance();
     private static Logger logger = Logger.getLogger(Main.class.getName());
+    static {
+	LogUtils.applyDefaultSettings(logger, Main.settings.REQUESTED_LOG_LEVEL);
+    }
 
     public static void main(String[] args) {
 	try {
