c732b66b832f812617e05326f6766e0e40f165bd
Sadat Chowdhury <sadatc@gmail.com>
2014-06-17 13:34:49 -0400

    added

---
+++ src/com/synthverse/synthscape/evolutionarymodel/alife/ALifeEvolutionSimulation.java
@@ -0,0 +1,13 @@
+package com.synthverse.synthscape.evolutionarymodel.alife;
+
+import com.synthverse.synthscape.evolutionarymodel.embodied.EmbodiedEvolutionSimulation;
+
+@SuppressWarnings("serial")
+public class ALifeEvolutionSimulation extends EmbodiedEvolutionSimulation {
+
+    public ALifeEvolutionSimulation(long seed) throws Exception {
+	super(seed);
+	// TODO Auto-generated constructor stub
+    }
+
+}
