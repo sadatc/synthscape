cb4669fcd773b59fbb37fcb0eceed1b113105dd7
Sadat Chowdhury <sadatc@gmail.com>
2014-06-20 09:07:20 -0400

    introduced unicast

---
+++ src/com/synthverse/synthscape/core/Unicast.java
@@ -0,0 +1,72 @@
+package com.synthverse.synthscape.core;
+
+/**
+ * An interaction that has a source agent, target agent, location, and a signal
+ * type
+ * 
+ * @author sadat
+ * 
+ */
+public class Unicast {
+
+    private Agent senderAgent;
+    private Agent receiverAgent;
+
+    private SignalType signalType;
+
+    private int stepClock;
+
+    public Unicast() {
+	this(null, null, SignalType.GENERIC, 0);
+    }
+
+    public Unicast(Agent senderAgent, Agent receiverAgent, SignalType signalType, int stepClock) {
+	this.senderAgent = senderAgent;
+	this.receiverAgent = receiverAgent;
+	this.signalType = signalType;
+	this.stepClock = stepClock;
+
+    }
+
+    public SignalType getSignalType() {
+	return signalType;
+    }
+
+    public void setSignalType(SignalType signalType) {
+	this.signalType = signalType;
+    }
+
+    public Agent getSenderAgent() {
+	return senderAgent;
+    }
+
+    public void setSenderAgent(Agent senderAgent) {
+	this.senderAgent = senderAgent;
+    }
+
+    public int getStepClock() {
+	return stepClock;
+    }
+
+    public void setStepClock(int stepClock) {
+	this.stepClock = stepClock;
+    }
+
+    public Agent getReceiverAgent() {
+        return receiverAgent;
+    }
+
+    public void setReceiverAgent(Agent receiverAgent) {
+        this.receiverAgent = receiverAgent;
+    }
+
+    @Override
+    public String toString() {
+	return "Unicast [senderAgent=" + senderAgent + ", receiverAgent=" + receiverAgent + ", signalType="
+		+ signalType + ", stepClock=" + stepClock + "]";
+    }
+
+    
+    
+    
+}
