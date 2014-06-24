7ed5e6859dd546bbe877cb3555769337baae9642
Sadat Chowdhury <sadatc@gmail.com>
2014-06-24 10:46:39 -0400

    commented extra debug

--- src/com/synthverse/stacks/InstructionTranslator.java
+++ src/com/synthverse/stacks/InstructionTranslator.java
@@ -225,10 +225,12 @@ public class InstructionTranslator {
 	sb.append(" instructions to the vocabulary...");
 	logger.info(sb.toString());
 
+	/*
 	for (Instruction instruction : instructions) {
 	    logger.info("ADDED INSTRUCTION: " + instruction);
 	}
 
+	*/
 	logger.info("<<< done adding.\n");
 
     }
