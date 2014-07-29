2fc8c5dbca6f9d7c453f77220063d44a649b434b
Sadat Chowdhury <sadatc@gmail.com>
2014-07-29 09:55:23 -0400

    added species comparator to help with maintaining species list in an order

---
+++ src/com/synthverse/synthscape/core/SpeciesComparator.java
@@ -0,0 +1,18 @@
+package com.synthverse.synthscape.core;
+
+import java.util.Comparator;
+
+public class SpeciesComparator implements Comparator<Species> {
+
+    @Override
+    public int compare(Species s1, Species s2) {
+	int result = 0;
+
+	if (s1 != null && s2 != null) {
+	    result = s1.getName().compareToIgnoreCase(s2.getName());
+	}
+
+	return result;
+    }
+
+}
