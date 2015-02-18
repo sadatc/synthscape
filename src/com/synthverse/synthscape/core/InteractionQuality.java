44a96e4f2d358ce9e6fa706c267b2ba8dd0b7674
Sadat Chowdhury <sadatc@gmail.com>
2015-02-17 21:59:58 -0500

    introduced levels to interaction quality

--- src/com/synthverse/synthscape/core/InteractionQuality.java
+++ src/com/synthverse/synthscape/core/InteractionQuality.java
@@ -1,5 +1,11 @@
 package com.synthverse.synthscape.core;
 
 public enum InteractionQuality {
-	HIGHEST, HIGH, MEDIUM, POOR;
+	HIGHEST(1.0), HIGH(0.75), MEDIUM(0.5), POOR(0.25);
+
+	private double level;
+
+	InteractionQuality(double level) {
+		this.level = level;
+	}
 }
