4c66847b060c0100f21510c7a74656d3a1fec3a6
Sadat Chowdhury <sadatc@gmail.com>
2015-02-19 21:18:06 -0500

    re-organized files

--- scp_penzias.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# create new jar and put it into penzias
-rm -f client/synthscape.jar
-ant/bin/ant jar
-scp -r client/synthscape.jar s.chowdhury@penzias:
-scp -r qsub_.sh s.chowdhury@penzias:
