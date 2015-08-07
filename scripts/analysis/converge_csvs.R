409ec38e352ece365acf4c333ec5cc456b617ceb
Sadat Chowdhury <sadatc@gmail.com>
2015-08-07 09:52:17 -0400

    added more functionality

--- scripts/analysis/converge_csvs.R
+++ scripts/analysis/converge_csvs.R
@@ -9,46 +9,54 @@ FIELDS_TO_CHECK_FOR_NORMALITY <- CSV_FIELDS_TO_GRAB[-1] # just skip generations
 
 ###### PROGRAM STARTS HERE
 
-directory <- "/Users/sadat/richness_data/data/rich_broad_bi"
-csvFiles <- list.files(directory,pattern="*perf_dat.csv")
+processCSVS <-function(directory) {
+	csvFiles <- list.files(directory,pattern="*perf_dat.csv")
 
-#aggregateData will contain the data from ALL csv files.
-aggregateData <- data.frame()
+	#aggregateData will contain the data from ALL csv files.
+	aggregateData <- data.frame()
 
-for(i in 1:length(csvFiles)){
-#for(i in 1:20){
-	csvFile <- paste(directory,csvFiles[i],sep="/") # concats
-	csvFileData <- read.csv(csvFile, header=TRUE)
+	aggregatedFiles <- 0
+	#for(i in 1:length(csvFiles)){
+	for(i in 1:10){
+		csvFile <- paste(directory,csvFiles[i],sep="/") # concats
+		csvFileData <- read.csv(csvFile, header=TRUE)
  
-  	# only gather data if there is at least MAX_GENERATIONS amount of rows
-  	if(nrow(csvFileData) >= MAX_GENERATIONS) { 
-  		csvFileData <- csvFileData[1:MAX_GENERATIONS,] # we trim off excess rows
-	 	fileData <- data.frame(EXPERIMENT=i,csvFileData[CSV_FIELDS_TO_GRAB])
-    	aggregateData <- rbind(aggregateData,fileData)    
-		print(paste("done appending...",csvFile))
+		# only gather data if there is at least MAX_GENERATIONS amount of rows
+		if(nrow(csvFileData) >= MAX_GENERATIONS) { 
+			csvFileData <- csvFileData[1:MAX_GENERATIONS,] # we trim off excess rows
+			fileData <- data.frame(EXPERIMENT=i,csvFileData[CSV_FIELDS_TO_GRAB])
+			aggregateData <- rbind(aggregateData,fileData)    
+			print(paste("done aggregating data from:",csvFiles[i]))
+			aggregatedFiles <- aggregatedFiles + 1 
+		}
+	}
+	print(paste("number of experiments aggregated:",aggregatedFiles))
 
-  	}
-}
-# aggregateData now contains a gigantic table of experiment x generation values
-# at this point we do two things: 1) check normality and 2) create summary table
+	# aggregateData now contains a gigantic table of experiment x generation values
+	# at this point we do two things: 1) check normality and 2) create summary table
 
-# 1) check normality
+	# 1) check normality
 
-summaryData <- data.frame()
+	summaryData <- data.frame()
 
-for(generation in 1:MAX_GENERATIONS) {
-	print(paste("averaging generation=",generation))
-	numDataCols <- ncol(aggregateData)
+	for(generation in 1:MAX_GENERATIONS) {
+		print(paste("averaging generation=",generation))
+		numDataCols <- ncol(aggregateData)
 
-	# pick the observations: they are from col 3 onwards...
-	observations <- aggregateData[aggregateData$GENERATION==generation,3:numDataCols]
+		# pick the observations: they are from col 3 onwards...
+		observations <- aggregateData[aggregateData$GENERATION==generation,3:numDataCols]
 
-	# find the means of these values
+		# find the means of these values
+		meanObservations <-lapply(observations,mean, na.rm=TRUE)
 	
-	meanObservations <-lapply(observations,mean, na.rm=TRUE)
-	
-	generationSummary <- data.frame(GENERATION=generation,meanObservations)
-
-	summaryData <-rbind(summaryData,generationSummary)
+		generationSummary <- data.frame(GENERATION=generation,meanObservations)
+		summaryData <-rbind(summaryData,generationSummary)
+	}
+	return(summaryData)
 }
-print(summaryData)
+	
+
+
+### Main Program
+#directory <- "/Users/sadat/richness_data/data/rich_broad_bi"
+print(processCSVS(directory))
