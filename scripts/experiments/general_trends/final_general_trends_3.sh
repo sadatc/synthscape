f5ccb3140d79125f879385a4af6aab39dabd64e3
Sadat Chowdhury <sadatc@gmail.com>
2015-12-10 08:44:12 -0500

    standardizing runs

--- scripts/experiments/general_trends/final_general_trends_3.sh
+++ scripts/experiments/general_trends/final_general_trends_3.sh
@@ -7,36 +7,56 @@
 # number of repeats
 SIMS=30 
 
-# island homo/hetero 
-	  qsub -N isl_hom_non_3ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int none -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_hom_tra_3ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int trail -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_hom_bro_3ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int broadcast -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_hom_uni_3ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int unicast_n -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh	  
 
-	  qsub -N isl_het_non_3ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int none -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_het_tra_3ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int trail -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_het_bro_3ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int broadcast -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_het_uni_3ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int unicast_n -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh	  
+# grab server param
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -server)
+    SERVER="$2"
+    shift 
+    ;;
+    *)
+	    echo "program needs -server (andy|penzias) "
+    	exit 1
+    ;;
+esac
+shift;
+done
+
+
+# island homo/hetero 
+	 qsub -N i_ho_n_3 -v SERVER=${SERVER},SIM_PARAMS="-model island -int none -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_ho_t_3 -v SERVER=${SERVER},SIM_PARAMS="-model island -int trail -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_ho_b_3 -v SERVER=${SERVER},SIM_PARAMS="-model island -int broadcast -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_ho_u_3 -v SERVER=${SERVER},SIM_PARAMS="-model island -int unicast_n -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh	 
+
+	 qsub -N i_he_n_3 -v SERVER=${SERVER},SIM_PARAMS="-model island -int none -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_he_t_3 -v SERVER=${SERVER},SIM_PARAMS="-model island -int trail -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_he_b_3 -v SERVER=${SERVER},SIM_PARAMS="-model island -int broadcast -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_he_u_3 -v SERVER=${SERVER},SIM_PARAMS="-model island -int unicast_n -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh	 
 
 # embodied homo/hetero
-	  qsub -N emb_hom_non_3ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int none -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_hom_tra_3ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int trail -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_hom_bro_3ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int broadcast -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_hom_uni_3ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int unicast_n -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh	  
+	 qsub -N e_ho_n_3 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int none -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_ho_t_3 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int trail -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_ho_b_3 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int broadcast -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_ho_u_3 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int unicast_n -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh	 
 
-	  qsub -N emb_het_non_3ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int none -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_het_tra_3ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int trail -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_het_bro_3ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int broadcast -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_het_uni_3ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int unicast_n -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh	  
+	 qsub -N e_he_n_3 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int none -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_he_t_3 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int trail -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_he_b_3 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int broadcast -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_he_u_3 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int unicast_n -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh	 
 
 # alife homo/hetero
-	  qsub -N alife_hom_non_3ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int none -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_hom_tra_3ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int trail -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_hom_bro_3ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int broadcast -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_hom_uni_3ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int unicast_n -species 'homogenous'  -unconstrained -repeat ${SIMS}" qsub_script.sh	  
+	 qsub -N a_ho_n_3 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int none -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_ho_t_3 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int trail -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_ho_b_3 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int broadcast -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_ho_u_3 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int unicast_n -species 'homogenous' -unconstrained -repeat ${SIMS}" qsub_script.sh	 
 
 
-	  qsub -N alife_het_non_3ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int none -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_het_tra_3ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int trail -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_het_bro_3ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int broadcast -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_het_uni_3ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int unicast_n -species 'detector,extractor,transporter'  -unconstrained -repeat ${SIMS}" qsub_script.sh	  
+	 qsub -N a_he_n_3 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int none -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_he_t_3 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int trail -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_he_b_3 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int broadcast -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_he_u_3 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int unicast_n -species 'detector,extractor,transporter' -unconstrained -repeat ${SIMS}" qsub_script.sh	 
--- scripts/experiments/general_trends/final_general_trends_4.sh
+++ scripts/experiments/general_trends/final_general_trends_4.sh
@@ -2,41 +2,60 @@
 
 # Author: Sadat Chowdhury (sadatc@gmail.com)
 # {island,embodied,alife} x {none,trail,broadcast,unicast} x {homo, hetero}
-# runs 24 scenarios for 3 task
+# runs 24 scenarios for 4 task
 
 # number of repeats
 SIMS=30 
 
-# island homo/hetero 
-	  qsub -N isl_hom_non_4ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int none -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_hom_tra_4ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int trail -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_hom_bro_4ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int broadcast -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_hom_uni_4ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int unicast_n -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh	  
+# grab server param
+while [[ $# > 0 ]]
+do
+key="$1"
 
-	  qsub -N isl_het_non_4ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int none -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_het_tra_4ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int trail -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_het_bro_4ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int broadcast -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N isl_het_uni_4ta -v SERVER=${SERVER},SIM_PARAMS="-model island -int unicast_n -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh	  
+case $key in
+    -server)
+    SERVER="$2"
+    shift 
+    ;;
+    *)
+	    echo "program needs -server (andy|penzias) "
+    	exit 1
+    ;;
+esac
+shift;
+done
+
+
+# island homo/hetero 
+	 qsub -N i_ho_n_4 -v SERVER=${SERVER},SIM_PARAMS="-model island -int none -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_ho_t_4 -v SERVER=${SERVER},SIM_PARAMS="-model island -int trail -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_ho_b_4 -v SERVER=${SERVER},SIM_PARAMS="-model island -int broadcast -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_ho_u_4 -v SERVER=${SERVER},SIM_PARAMS="-model island -int unicast_n -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh	 
+
+	 qsub -N i_he_n_4 -v SERVER=${SERVER},SIM_PARAMS="-model island -int none -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_he_t_4 -v SERVER=${SERVER},SIM_PARAMS="-model island -int trail -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_he_b_4 -v SERVER=${SERVER},SIM_PARAMS="-model island -int broadcast -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N i_he_u_4 -v SERVER=${SERVER},SIM_PARAMS="-model island -int unicast_n -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh	 
 
 # embodied homo/hetero
-	  qsub -N emb_hom_non_4ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int none -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_hom_tra_4ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int trail -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_hom_bro_4ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int broadcast -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_hom_uni_4ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int unicast_n -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh	  
+	 qsub -N e_ho_n_4 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int none -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_ho_t_4 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int trail -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_ho_b_4 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int broadcast -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_ho_u_4 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int unicast_n -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh	 
 
-	  qsub -N emb_het_non_4ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int none -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_het_tra_4ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int trail -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_het_bro_4ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int broadcast -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N emb_het_uni_4ta -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int unicast_n -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh	  
+	 qsub -N e_he_n_4 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int none -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_he_t_4 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int trail -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_he_b_4 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int broadcast -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N e_he_u_4 -v SERVER=${SERVER},SIM_PARAMS="-model embodied -int unicast_n -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh	 
 
 # alife homo/hetero
-	  qsub -N alife_hom_non_4ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int none -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_hom_tra_4ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int trail -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_hom_bro_4ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int broadcast -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_hom_uni_4ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int unicast_n -species 'homogenous'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh	  
+	 qsub -N a_ho_n_4 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int none -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_ho_t_4 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int trail -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_ho_b_4 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int broadcast -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_ho_u_4 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int unicast_n -species 'homogenous' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh	 
 
 
-	  qsub -N alife_het_non_4ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int none -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_het_tra_4ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int trail -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_het_bro_4ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int broadcast -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh
-	  qsub -N alife_het_uni_4ta -v SERVER=${SERVER},SIM_PARAMS="-model alife -int unicast_n -species 'detector,extractor,transporter'  -unconstrained -use_4_tasks  -repeat ${SIMS}" qsub_script.sh	  
+	 qsub -N a_he_n_4 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int none -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_he_t_4 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int trail -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_he_b_4 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int broadcast -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh
+	 qsub -N a_he_u_4 -v SERVER=${SERVER},SIM_PARAMS="-model alife -int unicast_n -species 'detector,extractor,transporter,processor' -unconstrained -use_4_tasks -repeat ${SIMS}" qsub_script.sh	 
