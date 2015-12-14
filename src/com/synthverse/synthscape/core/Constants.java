cc16d7ceb545b7ca620c0b1b5a39ca4cd9b03dff
Sadat Chowdhury <sadatc@gmail.com>
2015-12-13 23:17:47 -0500

    now reporting 6 figs

--- src/com/synthverse/synthscape/core/Constants.java
+++ src/com/synthverse/synthscape/core/Constants.java
@@ -87,7 +87,7 @@ public interface Constants {
 
 	public static final int DEFAULT_BITSET_SIZE = 1000000;
 
-	public static final String CSV_DOUBLE_FORMAT = "%.4f";
+	public static final String CSV_DOUBLE_FORMAT = "%.6f";
 	
 	public static final String EMPTY_STRING = "";
 	
