52244f4fbf1e5d520127e7886ce0517ed0f0b484
Sadat Chowdhury <sadatc@gmail.com>
2015-12-22 08:35:57 -0500

    added experiment 2 scripts

---
+++ scripts/experiments/interactions/iq_3t_10c.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Author: Sadat Chowdhury (sadatc@gmail.com)
+# {island,embodied,alife} x {none,trail,broadcast,unicast} x {homo, hetero}
+# runs 24 scenarios for 3 task using "run blocks" of 5 experiments each
+
+# grab server param
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -server)
+    S="$2"
+    shift 
+    ;;
+    *)
+	    echo "program needs -server (andy|penzias) "
+    	exit 1
+    ;;
+esac
+shift;
+done
+
+
+qsub -N e2_agts_3_10 -v S=${S},P="-experiment e2_agts_3_10" qsub_script.sh
+qsub -N e2_agth_3_10 -v S=${S},P="-experiment e2_agth_3_10" qsub_script.sh
+qsub -N e2_agtm_3_10 -v S=${S},P="-experiment e2_agtm_3_10" qsub_script.sh
+qsub -N e2_agtl_3_10 -v S=${S},P="-experiment e2_agtl_3_10" qsub_script.sh
+
+qsub -N e2_agbs_3_10 -v S=${S},P="-experiment e2_agbs_3_10" qsub_script.sh
+qsub -N e2_agbh_3_10 -v S=${S},P="-experiment e2_agbh_3_10" qsub_script.sh
+qsub -N e2_agbm_3_10 -v S=${S},P="-experiment e2_agbm_3_10" qsub_script.sh
+qsub -N e2_agbl_3_10 -v S=${S},P="-experiment e2_agbl_3_10" qsub_script.sh
+
+qsub -N e2_agus_3_10 -v S=${S},P="-experiment e2_agus_3_10" qsub_script.sh
+qsub -N e2_aguh_3_10 -v S=${S},P="-experiment e2_aguh_3_10" qsub_script.sh
+qsub -N e2_agum_3_10 -v S=${S},P="-experiment e2_agum_3_10" qsub_script.sh
+qsub -N e2_agul_3_10 -v S=${S},P="-experiment e2_agul_3_10" qsub_script.sh
+
+qsub -N e2_asts_3_10 -v S=${S},P="-experiment e2_asts_3_10" qsub_script.sh
+qsub -N e2_asth_3_10 -v S=${S},P="-experiment e2_asth_3_10" qsub_script.sh
+qsub -N e2_astm_3_10 -v S=${S},P="-experiment e2_astm_3_10" qsub_script.sh
+qsub -N e2_astl_3_10 -v S=${S},P="-experiment e2_astl_3_10" qsub_script.sh
+
+qsub -N e2_asbs_3_10 -v S=${S},P="-experiment e2_asbs_3_10" qsub_script.sh
+qsub -N e2_asbh_3_10 -v S=${S},P="-experiment e2_asbh_3_10" qsub_script.sh
+qsub -N e2_asbm_3_10 -v S=${S},P="-experiment e2_asbm_3_10" qsub_script.sh
+qsub -N e2_asbl_3_10 -v S=${S},P="-experiment e2_asbl_3_10" qsub_script.sh
+
+qsub -N e2_asus_3_10 -v S=${S},P="-experiment e2_asus_3_10" qsub_script.sh
+qsub -N e2_asuh_3_10 -v S=${S},P="-experiment e2_asuh_3_10" qsub_script.sh
+qsub -N e2_asum_3_10 -v S=${S},P="-experiment e2_asum_3_10" qsub_script.sh
+qsub -N e2_asul_3_10 -v S=${S},P="-experiment e2_asul_3_10" qsub_script.sh	
+
+
---
+++ scripts/experiments/interactions/iq_4t_10c.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Author: Sadat Chowdhury (sadatc@gmail.com)
+# {island,embodied,alife} x {none,trail,broadcast,unicast} x {homo, hetero}
+# runs 24 scenarios for 3 task using "run blocks" of 5 experiments each
+
+# grab server param
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -server)
+    S="$2"
+    shift 
+    ;;
+    *)
+	    echo "program needs -server (andy|penzias) "
+    	exit 1
+    ;;
+esac
+shift;
+done
+
+
+qsub -N e2_agts_4_10 -v S=${S},P="-experiment e2_agts_4_10" qsub_script.sh
+qsub -N e2_agth_4_10 -v S=${S},P="-experiment e2_agth_4_10" qsub_script.sh
+qsub -N e2_agtm_4_10 -v S=${S},P="-experiment e2_agtm_4_10" qsub_script.sh
+qsub -N e2_agtl_4_10 -v S=${S},P="-experiment e2_agtl_4_10" qsub_script.sh
+
+qsub -N e2_agbs_4_10 -v S=${S},P="-experiment e2_agbs_4_10" qsub_script.sh
+qsub -N e2_agbh_4_10 -v S=${S},P="-experiment e2_agbh_4_10" qsub_script.sh
+qsub -N e2_agbm_4_10 -v S=${S},P="-experiment e2_agbm_4_10" qsub_script.sh
+qsub -N e2_agbl_4_10 -v S=${S},P="-experiment e2_agbl_4_10" qsub_script.sh
+
+qsub -N e2_agus_4_10 -v S=${S},P="-experiment e2_agus_4_10" qsub_script.sh
+qsub -N e2_aguh_4_10 -v S=${S},P="-experiment e2_aguh_4_10" qsub_script.sh
+qsub -N e2_agum_4_10 -v S=${S},P="-experiment e2_agum_4_10" qsub_script.sh
+qsub -N e2_agul_4_10 -v S=${S},P="-experiment e2_agul_4_10" qsub_script.sh
+
+qsub -N e2_asts_4_10 -v S=${S},P="-experiment e2_asts_4_10" qsub_script.sh
+qsub -N e2_asth_4_10 -v S=${S},P="-experiment e2_asth_4_10" qsub_script.sh
+qsub -N e2_astm_4_10 -v S=${S},P="-experiment e2_astm_4_10" qsub_script.sh
+qsub -N e2_astl_4_10 -v S=${S},P="-experiment e2_astl_4_10" qsub_script.sh
+
+qsub -N e2_asbs_4_10 -v S=${S},P="-experiment e2_asbs_4_10" qsub_script.sh
+qsub -N e2_asbh_4_10 -v S=${S},P="-experiment e2_asbh_4_10" qsub_script.sh
+qsub -N e2_asbm_4_10 -v S=${S},P="-experiment e2_asbm_4_10" qsub_script.sh
+qsub -N e2_asbl_4_10 -v S=${S},P="-experiment e2_asbl_4_10" qsub_script.sh
+
+qsub -N e2_asus_4_10 -v S=${S},P="-experiment e2_asus_4_10" qsub_script.sh
+qsub -N e2_asuh_4_10 -v S=${S},P="-experiment e2_asuh_4_10" qsub_script.sh
+qsub -N e2_asum_4_10 -v S=${S},P="-experiment e2_asum_4_10" qsub_script.sh
+qsub -N e2_asul_4_10 -v S=${S},P="-experiment e2_asul_4_10" qsub_script.sh
+
+
