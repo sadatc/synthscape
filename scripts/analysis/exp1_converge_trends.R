1ef1d4b9f8a8adcd7c017861bb2c46c141a74b85
Sadat Chowdhury <sadatc@gmail.com>
2016-04-29 17:50:44 -0400

    new plots

---
+++ scripts/analysis/exp1_converge_trends.R
@@ -0,0 +1,358 @@
+# Constants
+library(data.table)
+
+
+MAX_GENERATIONS <-299
+
+CSV_FIELDS_TO_GRAB <- c("MODEL","RESOURCES","INTERACTIONS","SPECIES",
+	"GENERATION","CAPTURES_TOTAL","CAPTURES_BEST_CASE",
+	"CAPTURES_MEAN","TOT_FITNESS_MEAN","RATE_COMMUNICATION","RATE_MOTION",
+	"RES_E2C_STEPS_MEAN")
+
+CSV_FIELDS_TO_GRAB_S <- c("EXPERIMENT",CSV_FIELDS_TO_GRAB,"SIGNAL_SENT","SIGNAL_RECEIVED")
+
+EXPERIMENT_NUMBER <- 0
+
+MEAN_FILE_NAME <- "exp1_mean_trends"
+
+
+hashTable <- new.env(hash=TRUE)
+accumulateMean <- function(key, val) {
+	if(!is.nan(val)) {
+		countKey <- paste0(key,"_count")
+		meanKey <- paste0(key,"_mean")
+
+		if(is.null(hashTable[[countKey]])) {
+			# first entry		
+			hashTable[[countKey]] <- 1
+			hashTable[[meanKey]] <- val
+		} else {
+			# update mean
+			count <- hashTable[[countKey]]
+			currentMean <- hashTable[[meanKey]]
+			newCount <- count+1
+			newMean <- ((currentMean*count) + val)/newCount
+			hashTable[[countKey]] <- newCount
+			hashTable[[meanKey]] <- newMean
+		}	
+	}
+}
+
+getAccumulatedMean <- function(key) {	
+	meanKey <- paste0(key,"_mean")
+	if(!is.null(hashTable[[meanKey]])) {
+		return(hashTable[[meanKey]])
+	} else {
+		return(NA)
+	}
+}
+
+addSet <- function(setName, element) {
+	
+	setKey <- paste0(setName,"_set")
+
+	if(is.null(hashTable[[setKey]])) {
+		# first entry		
+		hashTable[[setKey]] <- element		
+	} else {
+		# update mean
+		elementStr <- hashTable[[setKey]]
+		elements <- unlist(strsplit(hashTable[[setKey]],":"))
+		found <- 0
+		for(e in elements) {
+			#print("iterating over")
+			#print(e)
+			if(element == e) {
+				found <- 1
+			}
+		}
+		if(found==0) {
+			newElement <- paste(elementStr,element,sep=":")
+			hashTable[[setKey]] <- newElement	
+		}	
+	}	
+	
+}
+
+getElements <- function(setName) {	
+	setKey <- paste0(setName,"_set")
+	if(!is.null(hashTable[[setKey]])) {
+		elements <- unlist(strsplit(hashTable[[setKey]],":"))
+		return(elements)
+	} 
+}
+
+###### PROGRAM STARTS HERE
+
+meanifyCSVS <-function(directory, aggregateData, howManyGenerations) {
+
+	csvFiles <- list.files(directory,pattern="*perf_dat.csv")
+	aggregatedFiles <- 0
+
+	MODEL <- ""	
+	SPECIES <- ""
+	INTERACTIONS <- ""
+	COMPLEXITY <- 0
+	CLONES <- 0
+
+	
+	for(i in 1:length(csvFiles)) {
+	#for(i in 1:2) {
+		assign("EXPERIMENT_NUMBER",EXPERIMENT_NUMBER + 1,.GlobalEnv)
+		csvFileName <- csvFiles[i]
+		correctedSpecies <- substr(csvFileName,5,5)
+		interactionType <- substr(csvFileName,6,6)
+		correctedModel <-substr(csvFileName,4,4)
+
+		
+		csvFile <- paste(directory,csvFileName,sep="/") # concats
+		print("reading data from: ")
+		print(csvFile)
+		csvFileData <- read.csv(csvFile, header=TRUE)
+		print("done reading...")		
+	
+	
+		# only gather data if there is at least MAX_GENERATIONS amount of rows
+
+		if(nrow(csvFileData) >= MAX_GENERATIONS) { 
+
+			if(interactionType == "b") {
+				CSV_FIELDS_TO_GRAB <- c(CSV_FIELDS_TO_GRAB,"TOT_BROADCAST_SENT",
+					"TOT_BROADCAST_RECEIVED")
+			}
+			
+			if(interactionType == "t") {
+				CSV_FIELDS_TO_GRAB <- c(CSV_FIELDS_TO_GRAB,"TOT_TRAIL_SENT",
+					"TOT_TRAIL_RECEIVED")
+			}
+
+			if(interactionType == "u") {
+				CSV_FIELDS_TO_GRAB <- c(CSV_FIELDS_TO_GRAB,"TOT_UNICAST_SENT",
+					"TOT_UNICAST_RECEIVED")
+			}
+
+			if(interactionType == "n") {
+				# nothing to do...
+			}
+
+			csvFileData <- csvFileData[1:MAX_GENERATIONS,] # we trim off excess rows
+			fileData <- data.frame(EXPERIMENT=i,csvFileData[CSV_FIELDS_TO_GRAB])
+			
+			## the data has some issues that needs to be fixed
+			
+			## problem 1: species column is always "s", we fix this by using 
+			# speciesType
+			fileData$SPECIES <- correctedSpecies
+			
+			
+			
+			if(interactionType == "b") {
+				fileData$SIGNAL_SENT <- fileData$TOT_BROADCAST_SENT 
+				fileData$SIGNAL_RECEIVED <- fileData$TOT_BROADCAST_RECEIVED
+				fileData$TOT_BROADCAST_SENT  <- NULL
+				fileData$TOT_BROADCAST_RECEIVED <- NULL
+			}
+			
+			if(interactionType == "t") {
+				fileData$SIGNAL_SENT <- fileData$TOT_TRAIL_SENT 
+				fileData$SIGNAL_RECEIVED <- fileData$TOT_TRAIL_RECEIVED
+				fileData$TOT_TRAIL_SENT  <- NULL
+				fileData$TOT_TRAIL_RECEIVED <- NULL
+			}
+
+			if(interactionType == "u") {
+				fileData$SIGNAL_SENT <- fileData$TOT_UNICAST_SENT 
+				fileData$SIGNAL_RECEIVED <- fileData$TOT_UNICAST_RECEIVED
+				fileData$TOT_UNICAST_SENT  <- NULL
+				fileData$TOT_UNICAST_RECEIVED <- NULL
+			}
+
+			if(interactionType == "n") {
+				fileData$SIGNAL_SENT <- 0
+				fileData$SIGNAL_RECEIVED <- 0
+			}
+
+
+
+			fileDataS <- data.frame(fileData[CSV_FIELDS_TO_GRAB_S])			
+			fileDataS <- head(fileDataS,howManyGenerations)
+			
+			
+			
+			# fix the captures best case and mean to percentages
+
+			fileDataS$CAPTURES_BEST_CASE <- 
+				fileDataS$CAPTURES_BEST_CASE/fileDataS$RESOURCES
+
+				
+			fileDataS$CAPTURES_MEAN <- 
+				fileDataS$CAPTURES_MEAN/fileDataS$RESOURCES
+			
+			
+			
+			numDataCols <- ncol(fileDataS)
+			
+			observations <- fileDataS[,6:numDataCols]
+
+			for(genI in 1:MAX_GENERATIONS) {
+				for(measureI in names(observations)) {
+					if(measureI!="GENERATION") {
+						# now form a gigantic key
+						theKey <- paste(correctedModel,correctedSpecies,interactionType,genI,measureI,sep=":")
+						addSet("MODEL",correctedModel)
+						addSet("SPECIES",correctedSpecies)
+						addSet("INTERACTIONS",interactionType)
+						theValue <- observations[observations$GENERATION==genI,measureI]
+						#print(theKey)
+						#print(theValue)
+						accumulateMean(theKey,theValue)
+						#print(getAccumulatedMean(theKey))
+					}
+					
+				}
+				#stop()
+				
+			}
+			
+			rm(fileDataS)
+			rm(fileData)
+			rm(csvFileData)
+			
+			#print(paste("done aggregating data from:",csvFiles[i]))
+			
+		}
+	}
+	
+}
+
+meanifyExp1Data <-function(dataDir, meanDir, howManyGenerations) {	
+
+	meanFile <- paste(meanDir,MEAN_FILE_NAME,sep="")
+	meanFile <- paste(meanFile,howManyGenerations,".csv",sep="")
+
+	if(file.exists(meanFile)) {
+		unlink(meanFile)
+		print(paste("previous meanFile was removed:",meanFile))
+	} else {
+		print("will create new meanFile")
+	}
+
+	
+	aggregateData <- data.frame()
+	
+	dataSubDirs <- list.dirs(dataDir,  full.names = FALSE)
+	print(dataSubDirs)
+	
+	aggregateData <- data.frame()
+
+	for(directoryIndex in 1:length(dataSubDirs)) {
+		dataSubDir <- dataSubDirs[directoryIndex]
+
+		if( dataSubDir != "") {
+
+			print(dataSubDir)
+			dataSubDirFullPath <- paste(dataDir,dataSubDir,sep="/")
+				
+
+			print("processing ...")
+			print(dataSubDirFullPath)	
+
+			
+			meanifyCSVS(dataSubDirFullPath,aggregateData, howManyGenerations)
+			
+			print("======")
+		}
+	}
+	print("done")
+
+
+	#print(getElements("MODEL"))
+	#print(getElements("SPECIES"))
+	#print(getElements("INTERACTIONS"))
+
+	aggregateDF <- data.frame()
+	for(model in getElements("MODEL")) {
+		for(species in getElements("SPECIES")) {
+			for(interactions in getElements("INTERACTIONS")) {
+				for(generations in 1:MAX_GENERATIONS) {
+					CAPTURES_MEAN <- paste(model,species,interactions,generations,"CAPTURES_MEAN",sep=":")
+					CAPTURES_MEAN <- getAccumulatedMean(CAPTURES_MEAN)
+
+					CAPTURES_BEST_CASE <- paste(model,species,interactions,generations,"CAPTURES_BEST_CASE",sep=":")
+					CAPTURES_BEST_CASE <- getAccumulatedMean(CAPTURES_BEST_CASE)
+
+					RES_E2C_STEPS_MEAN <- paste(model,species,interactions,generations,"RES_E2C_STEPS_MEAN",sep=":")
+					RES_E2C_STEPS_MEAN <- getAccumulatedMean(RES_E2C_STEPS_MEAN)
+
+					RATE_COMMUNICATION <- paste(model,species,interactions,generations,"RATE_COMMUNICATION",sep=":")
+					RATE_COMMUNICATION <- getAccumulatedMean(RATE_COMMUNICATION)
+
+					RATE_MOTION <- paste(model,species,interactions,generations,"RATE_MOTION",sep=":")
+					RATE_MOTION <- getAccumulatedMean(RATE_MOTION)
+
+
+					
+					
+					row <- data.frame(
+						MODEL=model,
+						SPECIES=species,
+						INTERACTIONS=interactions,
+						GENERATION=generations,
+						CAPTURES_BEST_CASE=CAPTURES_BEST_CASE,
+						CAPTURES_MEAN=CAPTURES_MEAN,
+						RES_E2C_STEPS_MEAN=RES_E2C_STEPS_MEAN,
+						RATE_MOTION=RATE_MOTION,
+						RATE_COMMUNICATION=RATE_COMMUNICATION
+					)
+					#print(names(row))					
+					aggregateDF <- rbind(aggregateDF,row)
+					#print(aggregateDF)
+					#print("came here!")
+				}
+			}
+		}
+	}
+	#print(aggregateDF)
+	write.csv(aggregateDF,file=meanFile,row.names=F)
+	
+	#write.csv(aggregateData,file=meanFile,row.names=F)
+
+	
+
+	
+
+}
+
+
+
+
+
+
+##############################   MAIN PROCESS BEGINS ###############################
+
+
+#summarizeExp1Data("/Users/sadat/ExperimentResults/GeneralTrends/penzias_4t/data/",
+#	"/Users/sadat/ExperimentResults/GeneralTrends/msiccSummaries")
+
+#summarizeExp1Data("/Users/sadat/ExperimentResults/GeneralTrends/andy_3t/data/",
+#	"/Users/sadat/ExperimentResults/GeneralTrends/msiccSummaries")
+
+#summarizeExp1Data("/Users/sadat/ExperimentResults/GeneralTrends/andy_3t/data/",
+#	"/Users/sadat/ExperimentResults/GeneralTrends/post_processed")
+
+#processCSVS("/Users/sadat/ExperimentResults/GeneralTrends/andy_3t/data/e1_ist_3_10",
+#	"/tmp/summ.csv")
+
+
+
+
+
+
+
+meanifyExp1Data("/Users/sadat/ExperimentResults/GeneralTrends/final_data/",
+	"/Users/sadat/analysis/data/exp1/", 300)	
+
+
+
+
+
